721cb27358d00df7d61c2c9daebd5813b262e529
vivek kumar guta <vivekkumar20005@gmail.com>
2026-08-07 15:55:36 -0500

    add enterprise integration package

---
+++ src/sdai/integrations/__init__.py
@@ -0,0 +1,10 @@
+from sdai.integrations.github import GitHubCli, GitHubIssue, PullRequestRequest
+from sdai.integrations.jira import JiraClient, JiraIssue
+
+__all__ = [
+    "GitHubCli",
+    "GitHubIssue",
+    "PullRequestRequest",
+    "JiraClient",
+    "JiraIssue",
+]
