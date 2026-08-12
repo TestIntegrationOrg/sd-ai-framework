7525864bb016a436c9de70087a48a902ecca203e
vivek kumar guta <vivekkumar20005@gmail.com>
2026-08-11 23:29:21 -0500

    feat: expose extension manifest API (#36)

---
+++ src/sdai/extensions/__init__.py
@@ -0,0 +1,21 @@
+from sdai.extensions.manifests import (
+    API_VERSION,
+    ExtensionKind,
+    ExtensionManifest,
+    ExtensionManifestError,
+    ExtensionMetadata,
+    load_extension_manifest,
+    parse_extension_manifest,
+    parse_extension_manifest_text,
+)
+
+__all__ = [
+    "API_VERSION",
+    "ExtensionKind",
+    "ExtensionManifest",
+    "ExtensionManifestError",
+    "ExtensionMetadata",
+    "load_extension_manifest",
+    "parse_extension_manifest",
+    "parse_extension_manifest_text",
+]
